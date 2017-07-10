ebbc169301c984ddb90f50f3801d2c39c9f4147b
LilyxFiore <liliana.fiorela@gmail.com>
2017-07-10 15:20:25 -0500

    cambios en index.html

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -33,24 +33,3 @@ const Header = () => {
     
     return header;
 };
-
-'use strict';
-
-
-const getJSON = (url, cb) => {
-
-  const xhr = new XMLHttpRequest();
-
-  xhr.addEventListener('load', () => {
-
-    if (xhr.status !== 200) {
-      return cb(new Error('Error loading JSON from ' + url + '(' + xhr.status + ')'));
-    }
-
-    cb(null, xhr.response);
-  });
-
-  xhr.open('GET', url);
-  xhr.responseType = 'json';
-  xhr.send();
-};
