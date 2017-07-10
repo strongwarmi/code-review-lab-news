4d9801d2b5c75ed9d50ee540c526aac1f631138f
strongwarmi <elianasc10@hotmail.com>
2017-07-10 13:04:14 -0500

    header complete (mobile y desktop)

---
+++ src/assets/js/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const render = (root) => {
+    //root.empty();
+    root.append(Header());
+    
+
+};
+
+const state = {
+    
+    screen: null
+
+};
+
+$(_ => {
+    const root = $('.root');
+    render(root);
+});
+
