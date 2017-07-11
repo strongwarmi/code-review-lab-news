ede1b468234e08291ae5a7515e4b2ed5f7bc8e8d
strongwarmi <elianasc10@hotmail.com>
2017-07-10 19:56:30 -0500

    detalle noticia y seccion mas popular

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -21,23 +21,22 @@ $(_ => {
 });
 
 /** agregando section cover **/
-const Cover = ()=>{
-  const containerCover = $('<section class="container"></section>');
-  const row = $('<div class="row"></div>');
-  const col = $('<div class="col-xs-12"><div class="cover__category">EDUCACIÓN</div><div class="cover__line"></div></div>');
-  const item = $('<div class="cover__title">' + news.title + '</div>' +
-      '<div class="row"><div class="col-xs-12 col-md-8 cover__subtitle">' + news.brief + '</div></div>' +
-      '<img src="assets/img/foto-n1.png" alt="" class="cover__img hidden-xs">');
+const Cover = () => {
+    const containerCover = $('<section class="container"></section>');
+    const row = $('<div class="row"></div>');
+    const col = $('<div class="col-xs-12"><div class="cover__category">EDUCACIÓN</div><div class="cover__line"></div></div>');
+    const item = $('<div class="cover__title">' + news.title + '</div>' +
+        '<div class="row"><div class="col-xs-12 col-md-8 cover__subtitle">' + news.brief + '</div></div>' +
+        '<img src="assets/img/foto-n1.png" alt="" class="cover__img hidden-xs">');
+    const line = $('<div class="col-xs-12 hidden-xs header-line"></div>');
+    
+    containerCover.append(row);
+    row.append(col);
+    col.append(item);
+    containerCover.append(line);
 
-  containerCover.append(row);
-  row.append(col);
-  col.append(item);
-
-  return containerCover;
+    return containerCover;
 };
-
-
-
 
 'use strict';
 const Header = () => {
@@ -60,13 +59,25 @@ const Header = () => {
 
 'use strict'
 const printNews = () => {
-  const secNews = $("<section class='container'><div class='row'>"+
-  "<div class='col-md-8 col-lg-8'>"+
-  "<div class='col-md-2 col-lg-2'>"+
-  "<img src='assets/img/authors/"+news.author.picture+"'></div>"+
-  "<div class='col-md-6 col-lg-6'><p>"+news.author.name+"</p>"+
-  "<p>"+ news['published-date'] +"</p><p>"+news.author.user+"</p></div></div></div>"+
-  "<div class='row'><div class='col-md-8'>"+news.body+"</div></div></section>");
+    const secNews = $('<section class="container"></section>');
+    const row = $('<div class="row"></div>');
+    const divNews = $('<div class="col-xs-12"></div>');
+    const noticia = $('<div class="col-sm-8"><div class="row"><div class="col-xs-4 col-sm-2"><img src="assets/img/authors/' + news.author.picture + '"></div><div class="col-xs-8 col-sm-6"><p>' + news.author.name + '<br>' + news['published-date'] + '<br>' + news.author.user + '</p></div></div><div class="row">' + news.body + '</div></div>');
 
-  return secNews;
+    const popular = $('<div class="col-sm-4"><h4 class="text-uppercase"><strong>notas relacionadas</strong></h4><div class="linea"></div><br>' +
+    '<div class="row"><div class="col-xs-6"><img class="img-responsive" src="assets/img/foto-n3.png"><span>Laboratoria 2017 <br> título noticia</span></div>' +
+    '<div class="col-xs-6"><img class="img-responsive" src="assets/img/foto-n4.png">     <span>Laboratoria 2017 <br> título noticia</span></div></div><br><br>' +
+    '<h4 class="text-uppercase"><strong>más populares</strong></h4><div class="linea"></div><br>' +
+    '<div class="popular"><div class="foto"><img src="assets/img/foto-n5.png"></div><div class="titulo"><p>Noticia más popular <br>Laboratoria Perú</p></div></div>' +
+    '<div class="popular"><div class="foto"><img src="assets/img/foto-n6.png"></div><div class="titulo"><p>Noticia popular <br>Laboratoria Perú</p></div></div>' +
+    '<div class="popular hidden-xs"><div class="foto"><img src="assets/img/foto-n7.png"></div><div class="titulo">     <p>Noticia más popular <br>Laboratoria Perú</p></div></div>' +
+    '<div class="popular hidden-xs"><div class="foto"><img src="assets/img/foto-n8.png"></div><div class="titulo">        <h4>Noticia más popular <br>Laboratoria Perú</h4></div></div>' +
+    '<div class="popular hidden-xs"><div class="foto"><img src="assets/img/foto-n9.png"></div><div class="titulo">          <h4>Noticia más popular <br>Laboratoria Perú</h4></div></div></div>');
+
+    row.append(noticia);
+    row.append(popular);
+    divNews.append(row)
+    secNews.append(divNews);
+
+    return secNews;
 }
--- src/assets/js/components/cover.js
+++ src/assets/js/components/cover.js
@@ -1,18 +1,17 @@
 /** agregando section cover **/
-const Cover = ()=>{
-  const containerCover = $('<section class="container"></section>');
-  const row = $('<div class="row"></div>');
-  const col = $('<div class="col-xs-12"><div class="cover__category">EDUCACIÓN</div><div class="cover__line"></div></div>');
-  const item = $('<div class="cover__title">' + news.title + '</div>' +
-      '<div class="row"><div class="col-xs-12 col-md-8 cover__subtitle">' + news.brief + '</div></div>' +
-      '<img src="assets/img/foto-n1.png" alt="" class="cover__img hidden-xs">');
+const Cover = () => {
+    const containerCover = $('<section class="container"></section>');
+    const row = $('<div class="row"></div>');
+    const col = $('<div class="col-xs-12"><div class="cover__category">EDUCACIÓN</div><div class="cover__line"></div></div>');
+    const item = $('<div class="cover__title">' + news.title + '</div>' +
+        '<div class="row"><div class="col-xs-12 col-md-8 cover__subtitle">' + news.brief + '</div></div>' +
+        '<img src="assets/img/foto-n1.png" alt="" class="cover__img hidden-xs">');
+    const line = $('<div class="col-xs-12 hidden-xs header-line"></div>');
+    
+    containerCover.append(row);
+    row.append(col);
+    col.append(item);
+    containerCover.append(line);
 
-  containerCover.append(row);
-  row.append(col);
-  col.append(item);
-
-  return containerCover;
+    return containerCover;
 };
-
-
-
